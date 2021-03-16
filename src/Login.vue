50dbfc34709bd7749d311f358bdefb0dc434a094
baism3 <baism3@asiainfo.com>
2021-03-16 12:00:32 +0800

    css 测试案例

---
+++ src/Login.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="login_container">
+  <div class="login_box"></div>
+  <div class="div1"></div>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login_box{
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 90%;
+  top: 90%;
+}
+.div1{
+  width: 100px;
+  height: 100px;
+  border: 10px solid red;
+  padding: 100%;
+  position: absolute;
+}
+</style>
